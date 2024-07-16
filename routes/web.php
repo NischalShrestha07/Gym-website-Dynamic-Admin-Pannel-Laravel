a2a78a50187e347a9799941578ab7d5fea0a3f20
Nischal <nischalshrestha800@gmail.com>
2024-07-16 12:46:43 +0545

    About the Admin Pannels

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,19 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\Frontend\ContactController;
 use App\Http\Controllers\Frontend\HomeController;
 use App\Http\Controllers\Frontend\TrainersController;
 use App\Http\Controllers\Frontend\WhyusController;
 use Illuminate\Support\Facades\Route;
 
+//Admin Routes
+Route::get('/admin', [AdminController::class, 'index']);
+
+
+
+
+//Customer Routes
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/whyus', [WhyusController::class, 'index']);
 Route::get('/trainer', [TrainersController::class, 'index']);
