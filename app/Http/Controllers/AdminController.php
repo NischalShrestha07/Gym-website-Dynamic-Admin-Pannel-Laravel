ac614a8c9e7af7f472293db37d6b3f73ad4fd4fa
Nischal <nischalshrestha800@gmail.com>
2024-07-17 09:30:27 +0545

    Commits

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Home;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class AdminController extends Controller
 {
@@ -52,4 +53,50 @@ class AdminController extends Controller
 
         return redirect()->back()->with('success', 'Home updated successfully');
     }
+
+
+
+    // public function update(Request $request, $id)
+    // {
+    //     Log::info('Update method called with ID: ' . $id);
+
+    //     $home = Home::find($id);
+    //     Log::info('Home found: ' . json_encode($home));
+
+    //     // Validate request
+    //     $request->validate([
+    //         'title' => 'required|string|max:255',
+    //         'HomeImage' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+    //         'Description' => 'required|string',
+    //         'ContactImage' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+    //     ]);
+
+    //     Log::info('Request validated successfully.');
+
+    //     // Update title and description
+    //     $home->title = $request->input('title');
+    //     $home->Description = $request->input('Description');
+
+    //     // Update Home Image
+    //     if ($request->hasFile('HomeImage')) {
+    //         $homeImage = $request->file('HomeImage');
+    //         $homeImageName = time() . '_' . $homeImage->getClientOriginalName();
+    //         $homeImage->move(public_path('uploads/homes'), $homeImageName);
+    //         $home->HomeImage = $homeImageName;
+    //     }
+
+    //     // Update Contact Image
+    //     if ($request->hasFile('ContactImage')) {
+    //         $contactImage = $request->file('ContactImage');
+    //         $contactImageName = time() . '_' . $contactImage->getClientOriginalName();
+    //         $contactImage->move(public_path('uploads/homes'), $contactImageName);
+    //         $home->ContactImage = $contactImageName;
+    //     }
+
+    //     $home->save();
+
+    //     Log::info('Home updated successfully.');
+
+    //     return redirect()->route('homes.index')->with('success', 'Home updated successfully');
+    // }
 }
