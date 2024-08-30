99add4064c0f5e0b4c6abfd7dbbb847ec6d84faf
Nischal <nischalshrestha800@gmail.com>
2024-08-30 11:25:59 +0545

    Managing Login and Authentication

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -42,11 +42,8 @@
             <div class="card-body">
                 <p class="login-box-msg">Sign in to start your session</p>
 
-                {{-- <form action="{{ route('admin.authenticate') }}" method="post"> --}}
-                <form action="" method="post">
+                <form action="{{ route('admin.authenticate') }}" method="post">
                     @csrf
-
-
                     <div class="input-group mb-3">
                         <input type="email" name="email" class="form-control" placeholder="Email">
                         <div class="input-group-append">
