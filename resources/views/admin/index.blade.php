e5ca2fe477d306229bcf92e1df8031481a18fa1b
Nischal <nischalshrestha800@gmail.com>
2024-11-29 23:48:40 +0545

    Managed the Errors

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -13,7 +13,7 @@
             <div class="col-md-12 grid-margin">
                 <div class="row">
                     <div class="col-12 col-xl-8 mb-4 mb-xl-0">
-                        <h3 class="font-weight-bold">WELCOME TO DAZZEL'S FAMILY</h3>
+                        <h3 class="font-weight-bold">WELCOME TO ROYAL POWER'S FAMILY</h3>
                         <h6 class="font-weight-normal mb-0">All systems are running smoothly! You have <span
                                 class="text-primary">3
                                 unread alerts!</span></h6>
@@ -84,7 +84,7 @@
             <div class="col-md-6 grid-margin stretch-card">
                 <div class="card">
                     <div class="card-body">
-                        <p class="card-title">DAZZEL GYM'S DETAILS</p>
+                        <p class="card-title">ROYAL POWER GYM'S DETAILS</p>
                         <p class="font-weight-500">The total number of sessions within the date range. It is the period
                             time a
                             user is actively engaged with your website, page or app, etc</p>
@@ -114,7 +114,7 @@
                 <div class="card">
                     <div class="card-body">
                         <div class="d-flex justify-content-between">
-                            <p class="card-title">DAZZEL GYM'S'S Report</p>
+                            <p class="card-title">ROYAL POWER GYM'S'S Report</p>
                             <a href="#" class="text-info">View all</a>
                         </div>
                         <p class="font-weight-500">The total number of sessions within the date range. It is the period
--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -128,20 +128,7 @@
                     </h2>
                     <p>
                         {{ $item->headdetail }}
-                        {{-- Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                        ut labore
-                        et
-                        dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                        aliquip ex
-                        ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillumLorem
-                        ipsum
-                        dolor
-                        sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-                        magna aliqua.
-                        Ut
-                        enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                        consequat.
-                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum --}}
+
                     </p>
                     <div class="btn-box">
                         <a href="">
