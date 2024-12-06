ea0d1f9483d76a1e7c032fa2cd998299f07d2604
Nischal <nischalshrestha800@gmail.com>
2024-12-06 08:58:35 +0545

    ksjd

--- resources/views/admin/register.blade.php
+++ resources/views/admin/register.blade.php
@@ -153,7 +153,7 @@
         </div>
     </div>
 
-    <!-- Bootstrap 5 JS Bundle with Popper -->
+    <!-- Bootstrap 5 JS Bundle with Poppers -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
